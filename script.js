0b19231c2de636e4a196891fbc0cd9442142d506
Qwertyuiop21672 <ben.r.peng@gmail.com>
2017-01-30 21:52:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var rld = 0
 var dps = 0
 function find_variables() {
     dmg = parseInt(prompt("Damage per Bullet:"));
+    
     rof = parseInt(prompt("Rate of Fire:"));
     cap = parseInt(prompt("Clip Capacity:"));
     rld = parseInt(prompt("Reload Time:"));
